dbea10c345c81d34630032afd92ca785628457e5
olumide748 <145693653+olumide748@users.noreply.github.com>
2024-01-29 13:35:22 +0100

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+// script.js
+document.getElementById('toggleBtn').addEventListener('click', function() {
+    document.getElementById('offCanvas').style.width = '250px'; // Adjust the width as needed
+  });
+  
+  document.getElementById('closeBtn').addEventListener('click', function() {
+    document.getElementById('offCanvas').style.width = '0';
+  });
+  
